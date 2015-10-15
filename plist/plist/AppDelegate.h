c470659c6fd23e5e7d0dabb471dcfcd891367650
LShawn <730356947@qq.com>
2015-10-15 12:43:11 +0800

    first

---
+++ plist/plist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  plist
+//
+//  Created by Shawn Li on 15/10/15.
+//  Copyright (c) 2015年 Shawn Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ plist/plist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  plist
+//
+//  Created by Shawn Li on 15/10/15.
+//  Copyright (c) 2015年 Shawn Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
